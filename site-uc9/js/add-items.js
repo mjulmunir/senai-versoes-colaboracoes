546aa0a6f9ed362fbfa4c5688216236b273b9ebf
mjulmunir <matvinatica@gmail.com>
2021-11-14 14:44:58 -0300

    finalizando site

--- site-uc9/js/add-items.js
+++ site-uc9/js/add-items.js
@@ -1,111 +1,138 @@
-var newProducts = document.getElementById("new-products");
-var featuredProducts = document.getElementById("featured-products");
-var media = document.getElementById("media");
+const newProducts = document.getElementById("new-products");
+const featuredProducts = document.getElementById("featured-products");
 
 const newProductsList = [
-  ["processor.png", "#", "Processador: ..., Marca: ..., Modelo: ...", "R$ ..."],
-  [
-    "graphics-card.jpg",
-    "#",
-    "Placa de vídeo, Marca: ..., Modelo: ...",
-    "R$ ...",
-  ],
-  ["tablet.jpg", "#", "Tablet, Marca: ...", "R$ ..."],
-  ["cpu-cooler.jpg", "#", "Cooler, Marca: ..., Tamanho: ...", "R$ ..."],
+	[
+		"processor.png",
+		"#",
+		"Processador: ..., Marca: ..., Modelo: ...",
+		"R$ ...",
+	],
+	[
+		"graphics-card.jpg",
+		"#",
+		"Placa de vídeo, Marca: ..., Modelo: ...",
+		"R$ ...",
+	],
+	["tablet.jpg", "#", "Tablet, Marca: ...", "R$ ..."],
+	["cpu-cooler.jpg", "#", "Cooler, Marca: ..., Tamanho: ...", "R$ ..."],
 ];
 
 const featuredProductsList = [
-  [
-    "all-in-one.png",
-    "#",
-    "Computador All In One, Marca ..., Processador ..., Memória ram: ...",
-    "R$ ...",
-  ],
-  [
-    "notebook.jpg",
-    "#",
-    "Notebook, Marca ..., Processador ..., Memória ram: ...",
-    "R$ ...",
-  ],
-  [
-    "monitor.png",
-    "#",
-    "Monitor, Marca ..., Resolução ..., Conexão: ...",
-    "R$ ...",
-  ],
-  ["smartphone.png", "#", "Smartphone, Marca: ...", "R$ ..."],
-  [
-    "headphone.png",
-    "#",
-    "Headphone, Marca: ..., Conexão: ..., Cor: ...",
-    "R$ ...",
-  ],
-  [
-    "keyboard.png",
-    "#",
-    "Teclado, Marca: ..., Conexão: ..., Cor: ...",
-    "R$ ...",
-  ],
-  ["mouse.jpg", "#", "Mouse, Marca: ..., Conexão: ..., DPI: ...", "R$ ..."],
-  [
-    "cabinet.jpg",
-    "#",
-    "Gabinete, Marca: ..., Cor: ..., Fator de forma: ...",
-    "R$ ...",
-  ],
-];
-
-const mediaList = [
-  ["playing.jpg", "https://www.facebook.com/"],
-  ["console.jpg", "https://www.instagram.com/"],
-  ["setup.jpg", "https://www.facebook.com/"],
+	[
+		"all-in-one.png",
+		"#",
+		"Computador All In One, Marca ..., Processador ..., Memória ram: ...",
+		"R$ ...",
+	],
+	[
+		"notebook.jpg",
+		"#",
+		"Notebook, Marca ..., Processador ..., Memória ram: ...",
+		"R$ ...",
+	],
+	[
+		"monitor.png",
+		"#",
+		"Monitor, Marca ..., Resolução ..., Conexão: ...",
+		"R$ ...",
+	],
+	["smartphone.png", "#", "Smartphone, Marca: ...", "R$ ..."],
+	[
+		"headphone.png",
+		"#",
+		"Headphone, Marca: ..., Conexão: ..., Cor: ...",
+		"R$ ...",
+	],
+	[
+		"keyboard.png",
+		"#",
+		"Teclado, Marca: ..., Conexão: ..., Cor: ...",
+		"R$ ...",
+	],
+	["mouse.jpg", "#", "Mouse, Marca: ..., Conexão: ..., DPI: ...", "R$ ..."],
+	[
+		"cabinet.jpg",
+		"#",
+		"Gabinete, Marca: ..., Cor: ..., Fator de forma: ...",
+		"R$ ...",
+	],
 ];
 
 const productsFolder = "img/products";
+
+listProducts(newProductsList, productsFolder, newProducts);
+listProducts(featuredProductsList, productsFolder, featuredProducts);
+
+function listProducts(itemList, itemSource, items) {
+	for (let i = 0; i < itemList.length; i++) {
+		let img = document.createElement("img");
+		let imgDiv = document.createElement("div");
+		let desc = document.createElement("p");
+		let price = document.createElement("p");
+		let infoDiv = document.createElement("div");
+		let a = document.createElement("a");
+		let col = document.createElement("div");
+
+		img.src = `${itemSource}/${itemList[i][0]}`;
+		img.alt = itemList[i][0];
+		img.className = "img-fluid";
+
+		imgDiv.className =
+			"image d-flex align-items-center justify-content-center";
+		imgDiv.appendChild(img);
+
+		desc.innerText = `${itemList[i][2]}`;
+		desc.className = "description";
+
+		price.innerText = `${itemList[i][3]}`;
+		price.className = "price";
+
+		infoDiv.className = "info text-center";
+		infoDiv.appendChild(desc);
+		infoDiv.appendChild(price);
+
+		a.href = `${itemList[i][1]}`;
+		a.className = "link rounded";
+		a.appendChild(imgDiv);
+		a.appendChild(infoDiv);
+
+		col.className = "col d-flex justify-content-center";
+		col.appendChild(a);
+
+		items.appendChild(col);
+	}
+}
+
+const media = document.getElementById("media");
+
+const mediaList = [
+	["playing.jpg", "https://www.facebook.com/"],
+	["console.jpg", "https://www.instagram.com/"],
+	["setup.jpg", "https://www.facebook.com/"],
+];
+
 const mediaFolder = "img/media";
 
-listItems(newProductsList, productsFolder, newProducts);
-listItems(featuredProductsList, productsFolder, featuredProducts);
-listItems(mediaList, mediaFolder, media);
+listMedia(mediaList, mediaFolder, media);
 
-function listItems(itemList, itemSource, items) {
-  for (let i = 0; i < itemList.length; i++) {
-    let a = document.createElement("a");
-    let div = document.createElement("div");
-    let img = document.createElement("img");
-    let imgDiv = document.createElement("div");
-    let description = document.createElement("p");
-    let descDiv = document.createElement("div");
-    let price = document.createElement("p");
-    let priceDiv = document.createElement("div");
+function listMedia(itemList, itemSource, items) {
+	for (let i = 0; i < itemList.length; i++) {
+		let div = document.createElement("div");
+		let a = document.createElement("a");
+		let img = document.createElement("img");
 
-    img.src = `${itemSource}/${itemList[i][0]}`;
-    img.alt = `${itemList[i][0]}`;
-    img.className = "img-fluid";
-    imgDiv.appendChild(img);
-    imgDiv.className = "product";
+		img.className = "image";
+		img.src = `${itemSource}/${itemList[i][0]}`;
+		img.alt = `${itemList[i][0]}`;
 
-    div.appendChild(imgDiv);
-    div.className = "p-3 d-flex align-items-center flex-column";
+		a.className = "link";
+		a.href = `${itemList[i][1]}`;
+		a.appendChild(img);
 
-    if (itemList[i].length > 2) {
-      description.innerText = itemList[i][2];
-      descDiv.appendChild(description);
-      descDiv.className = "product-info";
+		div.className = "col d-flex align-items-center justify-content-center";
+		div.appendChild(a);
 
-      price.innerText = itemList[i][3];
-      priceDiv.appendChild(price);
-      priceDiv.className = "product-info d-flex align-items-center";
-
-      div.appendChild(descDiv);
-      div.appendChild(priceDiv);
-    }
-
-    a.className = "col";
-    a.href = `${itemList[i][1]}`;
-
-    a.appendChild(div);
-
-    items.appendChild(a);
-  }
+		items.appendChild(div);
+	}
 }
