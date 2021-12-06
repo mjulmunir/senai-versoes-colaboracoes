243ee6d0cb17c24a5071f9a87849292113332f9f
mjulmunir <matvinatica@gmail.com>
2021-12-05 21:16:56 -0300

    finalizacao

--- site-uc9-angular/src/app/app.module.ts
+++ site-uc9-angular/src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { BannerComponent } from './components/banner/banner.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from "./components/banner/banner.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
-import { AccountComponent } from './pages/account/account.component';
-import { CardComponent } from './components/card/card.component';
-import { CardPromotionComponent } from './components/card-promotion/card-promotion.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
-import { InformationComponent } from './pages/information/information.component';
-import { PromotionComponent } from './pages/promotion/promotion.component';
+import { AccountComponent } from "./pages/account/account.component";
+import { CardComponent } from "./components/card/card.component";
+import { CardPromotionComponent } from "./components/card-promotion/card-promotion.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { InformationComponent } from "./pages/information/information.component";
+import { PromotionComponent } from "./pages/promotion/promotion.component";
 
 @NgModule({
   declarations: [
@@ -55,7 +54,6 @@ import { PromotionComponent } from './pages/promotion/promotion.component';
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatSidenavModule,
     MatToolbarModule,
     FormsModule,
   ],
---
+++ site-uc9-angular/src/app/navbar-items.ts
@@ -0,0 +1,10 @@
+export interface NavbarItem {
+  name: string;
+}
+
+export const NAVBAR_ITEM: NavbarItem[] = [
+  { name: "Todas as categorias" },
+  { name: "Novidades" },
+  { name: "Promoções" },
+  { name: "Kits" },
+];
